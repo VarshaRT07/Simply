c21c1863048eca6a03536b167f55d60ad144a28e
Varsha <varshathiru01@gmail.com>
2022-03-19 19:13:09 +0530

    nodemon

--- app.js
+++ app.js
@@ -9,4 +9,4 @@
 // require-function to use modules(commonjs)
 // module - info about current module(file)
 // process- info about env where the program is being executed
-console.log(__dirname);
+console.log("Hello");
